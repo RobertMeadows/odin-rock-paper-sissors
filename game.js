06d14b5292596c4a781d6c8846dc7afc2d715a7d
Robert Meadows <robertmeadows2007@gmail.com>
2022-05-15 17:35:37 -0400

    Add player and computer logic

---
+++ game.js
@@ -0,0 +1,60 @@
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    switch(playerSelection){
+        case "rock":
+            if (computerSelection == "rock") {
+                return "You tie! Rock bounces off of rock!";
+            } else if (computerSelection == "paper"){
+                return "You lose! Paper wraps rock!";
+            } else {
+                return "You win! Rock breaks scissors!";
+            }   
+        case "paper":
+            if (computerSelection == "rock") {
+                return "You win! Paper covers rock!";
+            } else if (computerSelection == "paper") {
+                return "You tie! Paper bounces off of paper!";
+            } else {
+                return "You lose! Scissors cut paper!";
+            }
+        case "scissors":
+            if (computerSelection == "rock") {
+                return "You lose! Rock breaks scissors";
+            } else if (computerSelection == "paper"){
+                return "You win! Scissors cut paper!";
+            } else {
+                return "You tie! Scissors bounce off of scissors!";
+            }
+    };
+    
+  }
+  
+ function computerPlay(){
+     let computerSelection;
+     
+     computerSelection = Math.floor((Math.random()*3)+1);
+     for (let i = 0; i < 5; i++) {
+        console.log(Math.floor((Math.random()*3)+1));
+      }
+     switch(computerSelection){
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+     };
+     
+ } 
+
+ function playerPlay (){
+     let playerEntry;
+
+     playerEntry = prompt("Please enter your weapon", "rock, paper, or scissors?");
+     playerEntry = playerEntry.toLowerCase();
+
+     return playerEntry;
+ }
+  const playerSelection = playerPlay();
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
